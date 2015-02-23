6b8489907e8061f64fa23113a65ff98792a10045
jing-olemiss <jwu2@olemiss.edu>
2015-02-22 18:18:33 -0600

    First commit
    download data and read data
    
    Signed-off-by: jing-olemiss <jwu2@olemiss.edu>

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+## download target from the given URL
+dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(dataURL, "SamsungGalaxyS.zip", method = "curl")
+
+#unzip datafile
+unzip("SamsungGalaxyS.zip")
+
+#read files
+url_test <- "UCI HAR Dataset/test/Inertial Signals/"
+url_train <- "UCI HAR Dataset/train/Inertial Signals/"
+body_acc_x_test <- read.table(paste(url_test,"body_acc_x_test.txt", sep = ""), header = FALSE)
+body_acc_y_test <- read.table(paste(url_test,"body_acc_y_test.txt", sep = ""), header = FALSE)
+body_acc_z_test <- read.table(paste(url_test,"body_acc_z_test.txt", sep = ""), header = FALSE)
+body_gyro_x_test <- read.table(paste(url_test,"body_gyro_x_test.txt", sep = ""), header = FALSE)
+body_gyro_y_test <- read.table(paste(url_test,"body_gyro_y_test.txt", sep = ""), header = FALSE)
+body_gyro_z_test <- read.table(paste(url_test,"body_gyro_z_test.txt", sep = ""), header = FALSE)
+total_acc_x_test <- read.table(paste(url_test,"total_acc_x_test.txt", sep = ""), header = FALSE)
+total_acc_y_test <- read.table(paste(url_test,"total_acc_y_test.txt", sep = ""), header = FALSE)
+total_acc_z_test <- read.table(paste(url_test,"total_acc_z_test.txt", sep = ""), header = FALSE)
+body_acc_x_train <- read.table(paste(url_train,"body_acc_x_train.txt", sep = ""), header = FALSE)
+body_acc_y_train <- read.table(paste(url_train,"body_acc_y_train.txt", sep = ""), header = FALSE)
+body_acc_z_train <- read.table(paste(url_train,"body_acc_z_train.txt", sep = ""), header = FALSE)
+body_gyro_x_train <- read.table(paste(url_train,"body_gyro_x_train.txt", sep = ""), header = FALSE)
+body_gyro_y_train <- read.table(paste(url_train,"body_gyro_y_train.txt", sep = ""), header = FALSE)
+body_gyro_z_train <- read.table(paste(url_train,"body_gyro_z_train.txt", sep = ""), header = FALSE)
+total_acc_x_train <- read.table(paste(url_train,"total_acc_x_train.txt", sep = ""), header = FALSE)
+total_acc_y_train <- read.table(paste(url_train,"total_acc_y_train.txt", sep = ""), header = FALSE)
+total_acc_z_train <- read.table(paste(url_train,"total_acc_z_train.txt", sep = ""), header = FALSE)
