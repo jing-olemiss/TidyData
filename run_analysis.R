88bb1efa8a5e32055020efc658a01ca7a2cd5e16
jing-olemiss <jwu2@olemiss.edu>
2015-02-22 18:39:01 -0600

    Update
    Merge test and train data
    
    Signed-off-by: jing-olemiss <jwu2@olemiss.edu>

--- run_analysis.R
+++ run_analysis.R
@@ -26,3 +26,17 @@ body_gyro_z_train <- read.table(paste(url_train,"body_gyro_z_train.txt", sep = "
 total_acc_x_train <- read.table(paste(url_train,"total_acc_x_train.txt", sep = ""), header = FALSE)
 total_acc_y_train <- read.table(paste(url_train,"total_acc_y_train.txt", sep = ""), header = FALSE)
 total_acc_z_train <- read.table(paste(url_train,"total_acc_z_train.txt", sep = ""), header = FALSE)
+
+#merge test and train data
+body_acc_x <- merge(body_acc_x_test, body_acc_x_train, all = TRUE)
+body_acc_y <- merge(body_acc_y_test, body_acc_y_train, all = TRUE)
+body_acc_z <- merge(body_acc_z_test, body_acc_z_train, all = TRUE)
+body_gyro_x <- merge(body_gyro_x_test, body_gyro_x_train, all = TRUE)
+body_gyro_y <- merge(body_gyro_y_test, body_gyro_y_train, all = TRUE)
+body_gyro_z <- merge(body_gyro_z_test, body_gyro_z_train, all = TRUE)
+total_acc_x <- merge(total_acc_x_test, total_acc_x_train, all = TRUE)
+total_acc_y <- merge(total_acc_y_test, total_acc_y_train, all = TRUE)
+total_acc_z <- merge(total_acc_z_test, total_acc_z_train, all = TRUE)
+
+
+
